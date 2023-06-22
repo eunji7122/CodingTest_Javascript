84902cb4e4defd508eca310c5242d802fb5a3056
ejlee <ejlee@juvis.co.kr>
2023-06-22 17:51:18 +0900

    fix

--- programmers/Level2/RemovePairs.js
+++ programmers/Level2/RemovePairs.js
@@ -15,17 +15,3 @@ function solution(s) {
 
 console.log(solution("baabaa")); // 1
 console.log(solution("cdcd")); // 0
-
-// var answer = -1;
-// let filtering = true
-// const stack = [];
-//
-// for( let i of s ){
-//   stack.push(i)
-//   if(stack[stack.length-1] == stack[stack.length-2] ){
-//     stack.pop()
-//     stack.pop()
-//   }
-// }
-// answer = stack.length == 0 ? 1 : 0;
-// return answer;
