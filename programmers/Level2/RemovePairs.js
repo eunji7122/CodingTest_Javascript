5e37d37ed0b2a82a9d8d668feab47715ac9ff56c
ejlee <ejlee@juvis.co.kr>
2023-06-22 17:43:36 +0900

    fix

--- programmers/Level2/RemovePairs.js
+++ programmers/Level2/RemovePairs.js
@@ -1,18 +1,31 @@
 // 짝지어 제거하기
 function solution(s) {
-  let strArr = s.split("");
-  let index = 0;
-  while (index < strArr.length) {
-    if (strArr[index] === strArr[index + 1]) {
-      strArr.splice(index, 2);
-      index = 0;
-    } else {
-      index++;
+  const stack = [];
+
+  for (const item of s) {
+    stack.push(item);
+    if (stack[stack.length - 1] === stack[stack.length - 2]) {
+      stack.pop();
+      stack.pop();
     }
   }
 
-  return strArr.length === 0 ? 1 : 0;
+  return stack.length === 0 ? 1 : 0;
 }
 
 console.log(solution("baabaa")); // 1
 console.log(solution("cdcd")); // 0
+
+// var answer = -1;
+// let filtering = true
+// const stack = [];
+//
+// for( let i of s ){
+//   stack.push(i)
+//   if(stack[stack.length-1] == stack[stack.length-2] ){
+//     stack.pop()
+//     stack.pop()
+//   }
+// }
+// answer = stack.length == 0 ? 1 : 0;
+// return answer;
