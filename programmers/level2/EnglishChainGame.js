962a7364f1c1287d1f6f92668d4dccdb33bf5da2
eunji <lej7122@gmail.com>
2023-06-23 23:53:51 +0900

    0623

---
+++ programmers/level2/EnglishChainGame.js
@@ -0,0 +1,62 @@
+// 영어 끝말잇기
+function solution(n, words) {
+    let personNumber = 0;
+    let turnCount = 0;
+    let preWord = "";
+    let wordArr = [];
+    for (let i = 0; i < words.length; i++) {
+        if (i % n === 0) {
+            turnCount++;
+            personNumber = 1;
+        }
+        if (wordArr.indexOf(words[i]) !== -1) {
+            // 중복 체크
+            return [personNumber, turnCount];
+        }
+
+        let currentWord = words[i][0];
+        if (preWord !== "" && currentWord !== preWord) {
+            // 끝말잇기 체크
+            return [personNumber, turnCount];
+        }
+        preWord = words[i][words[i].length - 1];
+
+        wordArr.push(words[i]);
+        personNumber++;
+    }
+
+    return [0, 0];
+}
+
+console.log(
+    solution(3, [
+        "tank",
+        "kick",
+        "know",
+        "wheel",
+        "land",
+        "dream",
+        "mother",
+        "robot",
+        "tank",
+    ])
+); // [3, 3]
+console.log(
+    solution(5, [
+        "hello",
+        "observe",
+        "effect",
+        "take",
+        "either",
+        "recognize",
+        "encourage",
+        "ensure",
+        "establish",
+        "hang",
+        "gather",
+        "refer",
+        "reference",
+        "estimate",
+        "executive",
+    ])
+); // [0, 0]
---
+++ programmers/level2/JumpAndTeleport.js
@@ -0,0 +1,17 @@
+// 점프와 순간 이동
+function solution(n) {
+    let ans = 0;
+
+    while (n >= 1) {
+        if (n % 2 === 1) {
+            ans++;
+        }
+        n = parseInt(n / 2);
+    }
+
+    return ans;
+}
+
+console.log(solution(5)); // 2
+console.log(solution(6)); // 2
+console.log(solution(5000)); // 5
